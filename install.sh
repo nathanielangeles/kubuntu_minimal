7f8cf9e8870fa5faa0e121051e6ce31e0cd45c4a
Nathaniel Angeles <166530667+nathanielangeles@users.noreply.github.com>
2025-07-22 23:03:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,65 +1,63 @@
 #!/usr/bin/env bash
 
 # A bash script that installs all the essential software for my work
-# OS: Lubuntu 24.04.2 LTS
+# OS: Ubuntu MATE 24.04.2 LTS
 # Author: nathanielangeles
 # Date: July 22, 2025
 
 # Check for root privileges
 if [[ $EUID -ne 0 ]]; then
-    echo -e "❌ This script must be run with sudo:\nUsage: sudo ${0}"
+    echo -e "[-] This script must be run with sudo:\nUsage: sudo ${0}"
     exit 1
 fi
 
 # Update and upgrade the system
-echo -e "\n📦 Updating system..."
+echo -e "\n[*] Updating system..."
 apt update && apt upgrade -y
 
 # Install APT packages
-echo -e "\n📥 Installing APT packages..."
-apt install -y curl wget tlp terminator cherrytree syncthingtray wireshark remmina qbittorrent timeshift lxappearance fonts-noto papirus-icon-theme pavucontrol
+echo -e "\n[*] Installing APT packages..."
+apt install -y curl wget tlp terminator cherrytree syncthingtray wireshark remmina qbittorrent timeshift papirus-icon-theme flatpak
 
 # Enable & start TLP
 systemctl enable tlp
 systemctl start tlp
 
 # Install Flatpak and Flathub
-echo -e "\n📦 Installing Flatpak and adding Flathub repo..."
-apt install -y flatpak
+echo -e "\n[*] Adding Flathub repo..."
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install Flatpak apps
-echo -e "\n📥 Installing Flatpak apps..."
+echo -e "\n[*] Installing Flatpak apps..."
 flatpak install -y --noninteractive flathub \
     io.github.fizzyizzy05.binary \
     com.rafaelmardojai.Blanket \
     dev.geopjr.Calligraphy \
     me.iepure.devtoolbox \
     com.discordapp.Discord \
-    org.mozilla.Firefox \
-    org.kde.Francis \
+    org.mozilla.firefox \
+    org.kde.francis \
     io.missioncenter.MissionCenter \
     com.obsproject.Studio \
     md.obsidian.Obsidian \
-    io.gitlab.alescouto.password \
+    io.gitlab.elescoute.password \
     io.github.alainm23.planify \
     com.rustdesk.RustDesk \
     org.vinegarhq.Sober \
     com.spotify.Client \
     com.visualstudio.code \
-    io.github.flathub.Warehouse \
-    app.zen_browser.zen
-
+    io.github.flattool.Warehouse \
+    
 # Install Tailscale
-echo -e "\n🌐 Installing Tailscale..."
+echo -e "\n[*] Installing Tailscale..."
 curl -fsSL https://tailscale.com/install.sh | sh
 
 # Clone RedTeamify repo and run installer
-echo -e "\n🛠️ Cloning and running RedTeamify..."
+echo -e "\n🛠[*] Cloning and running RedTeamify..."
 mkdir -p ~/misc && cd ~/misc
 git clone https://github.com/nathanielangeles/redteamify.git
 cd redteamify
 chmod +x RedTeamify.sh
 ./RedTeamify.sh
 
-echo -e "\n✅ All installations are complete. Please reboot to apply all changes.\n"
+echo -e "\n[+] All installations are complete. Please reboot to apply all changes.\n"
